5c515fded1e53aa0aa23b3badbe28ba85a66ae9c
Nan Zhu <zhunansjtu@gmail.com>
2017-01-05 15:37:54 -0800

    fix

--- examples/src/main/scala/com/microsoft/spark/streaming/examples/test/TestReceiver.scala
+++ examples/src/main/scala/com/microsoft/spark/streaming/examples/test/TestReceiver.scala
@@ -35,6 +35,7 @@ object TestReceiver {
 
     for (receiverId <- 0 until totalReceiverNum) {
       for (partitionId <- 0 until 32) {
+        println(s"starting receiver $receiverId partition $partitionId")
         val startOffset: Long = receiverId * rate
         val receiver = EventHubsClientWrapper.getEventHubClient(
             Map(
