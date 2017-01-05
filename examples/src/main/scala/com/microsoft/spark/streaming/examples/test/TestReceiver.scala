4d81c11b2e0da31ccd31b23942654ad0c0e601ed
Nan Zhu <zhunansjtu@gmail.com>
2017-01-05 15:18:22 -0800

    upload test receiver

---
+++ examples/src/main/scala/com/microsoft/spark/streaming/examples/test/TestReceiver.scala
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.spark.streaming.examples.test
+
+import scala.collection.mutable.ListBuffer
+
+import com.microsoft.azure.eventhubs.EventData
+
+import org.apache.spark.streaming.eventhubs.EventHubsClientWrapper
+
+object TestReceiver {
+
+  def main(args: Array[String]): Unit = {
+    val totalReceiverNum = args(0).toInt
+    val rate = args(1).toInt
+    val namespace = args(2)
+    val name = args(3)
+    val policyName = args(4)
+    val policyKey = args(5)
+
+    for (receiverId <- 0 until totalReceiverNum) {
+      val startOffset: Long = receiverId * rate
+      for (partitionId <- 0 until 32) {
+        val receiver = EventHubsClientWrapper.getEventHubClient(
+            Map(
+              "eventhubs.namespace" -> namespace,
+              "eventhubs.partition.count" -> "32",
+              "eventhubs.name" -> name,
+              "eventhubs.policyname" -> policyName,
+              "eventhubs.policykey" -> policyKey,
+              "eventhubs.consumergroup" -> "$Default"),
+            partitionId,
+            startOffset,
+            rate)
+        val receivedBuffer = new ListBuffer[EventData]
+        var cnt = 0
+        while (receivedBuffer.length < rate && cnt < rate) {
+          cnt += 1
+          receivedBuffer ++= receiver.receive(rate - receivedBuffer.length)
+        }
+        receiver.close()
+      }
+    }
+  }
+}
