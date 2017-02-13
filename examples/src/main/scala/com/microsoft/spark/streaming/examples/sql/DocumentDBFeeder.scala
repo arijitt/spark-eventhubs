256586b197444eb832e7c510367e6e328ef12ce5
Nan Zhu <zhunansjtu@gmail.com>
2017-02-13 09:34:02 -0800

    example

--- examples/src/main/scala/com/microsoft/spark/streaming/examples/sql/DocumentDBFeeder.scala
+++ examples/src/main/scala/com/microsoft/spark/streaming/examples/sql/DocumentDBFeeder.scala
@@ -17,89 +17,45 @@
 
 package com.microsoft.spark.streaming.examples.sql
 
-import scala.io.Source
-
-import com.microsoft.azure.documentdb._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.ProcessingTime
 
 object DocumentDBFeeder {
 
 
   def main(args: Array[String]): Unit = {
 
-    val END_POINT = args(0)
-    val MASTER_KEY = args(1)
-    val DATABASE_ID = args(2)
-    val TEMP_COLLECTION_ID = args(3)
-    val MAIN_COLLECTION_ID = args(5)
+    val spark = SparkSession
+      .builder
+      .appName("StructuredNetworkWordCount")
+      .getOrCreate()
 
-    val docDBClient = new DocumentClient(END_POINT, MASTER_KEY, ConnectionPolicy.GetDefault(),
-      ConsistencyLevel.BoundedStaleness)
+    import spark.implicits._
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
 
-    val requestOptions = new RequestOptions()
-    requestOptions.setOfferThroughput(1000)
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
 
-    val stream = getClass.getResourceAsStream("/bulkimport.js")
-    val bulkImport = Source.fromInputStream(stream).getLines().foldLeft("")(
-      (str, line) => str + line + "\n")
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
 
-    println(bulkImport)
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .trigger(ProcessingTime("10 seconds"))
+      .format("docDB")
+      .option("docDB.endpoint", args(0))
+      .option("docDB.masterKey", args(1))
+      .option("docDB.databaseId", args(2))
+      .option("docDB.collectionId", args(3))
+      .option("docDB.procedureId", args(4))
+      .start()
 
-    val remoteProcedure = docDBClient.queryStoredProcedures(
-      "dbs/" + DATABASE_ID + "/colls/" + TEMP_COLLECTION_ID,
-      new SqlQuerySpec("SELECT * FROM root r WHERE r.id=@id",
-        new SqlParameterCollection(new SqlParameter(
-          "@id", "prod-1"))), null).getQueryIterable.toList.get(0)
-
-    docDBClient.deleteStoredProcedure(remoteProcedure.getSelfLink, null)
-
-    docDBClient.deleteCollection("dbs/" + DATABASE_ID + "/colls/" + TEMP_COLLECTION_ID, null)
-
-    val tempDocCollection = new DocumentCollection()
-    tempDocCollection.setId(TEMP_COLLECTION_ID)
-    docDBClient.createCollection("dbs/" + DATABASE_ID, tempDocCollection, null)
-
-    val newProcedure = new StoredProcedure()
-    newProcedure.setId("prod-1")
-    newProcedure.setBody(bulkImport)
-
-    val newlyCreatedProcedure = docDBClient.createStoredProcedure(
-      "dbs/" + DATABASE_ID + "/colls/" + TEMP_COLLECTION_ID, newProcedure, null).getResource
-
-    val docListBuffer = new Array[Object](args(4).toInt)
-
-    val batchId = System.currentTimeMillis()
-
-    for (i <- 0 until args(4).toInt) {
-      val allenDocument = new Document(s"""{\"name\":\"John\",\"batchId\":\"$batchId\"}""")
-      allenDocument.setId(i.toString)
-      docListBuffer(i) = allenDocument
-    }
-
-    val k = docListBuffer.map(doc => doc.asInstanceOf[JsonSerializable].toJson())
-
-    // Array.fill[Object](k.length + 1)
-    val parameters = new Array[Object](k.length + 1)
-    for (id <- k.indices) {
-      parameters(id) = k
-    }
-    parameters(parameters.length - 1) = true.asInstanceOf[AnyRef]
-
-    docDBClient.executeStoredProcedure(newlyCreatedProcedure.getSelfLink, parameters)
-
-    val mainCollections = docDBClient.queryCollections("dbs/" + DATABASE_ID,
-      new SqlQuerySpec("SELECT * FROM root r WHERE r.id=@id",
-        new SqlParameterCollection(new SqlParameter(
-          "@id", MAIN_COLLECTION_ID))),
-      null).getQueryIterable.toList
-
-    if (mainCollections.size() > 0) {
-      mainCollections.get(0)
-    } else {
-      val documentCollection = new DocumentCollection()
-      documentCollection.setId(MAIN_COLLECTION_ID)
-      docDBClient.createCollection("dbs/" + DATABASE_ID, documentCollection, null).getResource
-    }
-
-
+    query.awaitTermination()
   }
 }
